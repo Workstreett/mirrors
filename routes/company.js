d21ae01c738893eb39cddbbb1dc64fa2ca70c617
Pradeep Sharma <pradeep.sharma.cd.mec19@itbhu.ac.in>
2022-01-24 14:06:59 +0530

    Added Some routes to add user and company

--- routes/company.js
+++ routes/company.js
@@ -24,4 +24,57 @@ router.post("/get/all", Authenticate, (req, res) => {
 	});
 });
 
+router.post("/add/", (req, res) => {
+	Company.find({ name: req.body.name, role: req.body.role }, (err, data) => {
+		// console.log(err, data);
+		if (err) {
+			console.log(
+				"While calling company/add/ Error arised ",
+				err.message
+			);
+			res.status(503).send(err.message);
+		}
+		if (data) {
+			res.status(200).send(data);
+		} else {
+			const new_company = new Company({
+				name: req.body.name,
+				desc: req.body.desc,
+				logo: req.body.logo,
+				role: req.body.role,
+				tnor: req.body.tnor | 0,
+			});
+			new_company
+				.save()
+				.then((data) => {
+					res.status(200).send(data);
+				})
+				.catch((err) => {
+					console.log(
+						"While calling company/add/ Error arised ",
+						err.message
+					);
+					res.status(503).send(err.message);
+				});
+		}
+	});
+});
+
+router.post("/edit/byId", (req, res) => {
+	Company.findByIdAndUpdate(
+		req.body.Id,
+		{ desc: req.body.desc, logo: req.body.logo },
+		(err, data) => {
+			if (err) {
+				console.log(
+					"While calling company/edit/byId/ Error arised ",
+					err.message
+				);
+				res.status(503).send(err.message);
+			}
+			res.status(200).send("The Data has mutated Successfully");
+		}
+	);
+});
+
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -14,4 +14,53 @@ router.post("/get/byId", Authenticate, (req, res) => {
 	});
 });
 
+router.post("/add/", async (req, res) => {
+	// It requires Company Id, email, name
+	const roundObj = {
+		icon: req.body.icon || "fas fa-user",
+		name: req.body.roundName || "Resume Round",
+		status: req.body.status || 0,
+		remark: req.body.remark,
+	};
+	const obj = {
+		companyId: req.body.companyId,
+		status: req.body.companyStatus,
+		round: [roundObj],
+	};
+
+	try {
+		// If the users exist with the companyId then update
+		let user_data = await User.findOneAndUpdate(
+			{
+				email: req.body.email,
+				"appliedFor.companyId": req.body.companyId,
+			},
+			{ $push: { "appliedFor.$.round": roundObj } }
+		);
+		if (user_data) {
+			res.status(200).send("The user has been added Successfully!!");
+			return;
+		}
+		// If the companyId does not exist but the user does
+		user_data = await User.findOneAndUpdate(
+			{ email: req.body.email },
+			{ $push: { appliedFor: obj } }
+		);
+		if (user_data) {
+			res.status(200).send("The user has been added Successfully!!");
+			return;
+		}
+		user_data = new User({
+			name: req.body.name,
+			email: req.body.email,
+			appliedFor: [obj],
+		});
+		user_data.save();
+		res.status(200).send("The user has been added Successfully!!");
+	} catch (err) {
+		console.log(err.message);
+		res.status(503).send("Server Side Error");
+	}
+});
+
 module.exports = router;
