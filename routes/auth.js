0e80a7cba861ce00d1ebda5c4d1bdba34346e948
Pradeep Sharma <68096528+pradeepsh2203@users.noreply.github.com>
2022-03-14 13:23:31 +0530

    Added a try catch block in auth.js (#7)

--- routes/auth.js
+++ routes/auth.js
@@ -9,21 +9,26 @@ router.post(
 	"/google/callback",
 	passport.authenticate("googlePlus", { session: false }),
 	(req, res) => {
-		const token = JWT.sign(
-			{
-				iss: "Workstreet",
-				sub: req.user.id,
-			},
-			process.env.JSON_SECRET,
-			{ expiresIn: "1day" }
-		);
+		try {
+			const token = JWT.sign(
+				{
+					iss: "Workstreet",
+					sub: req.user.id,
+				},
+				process.env.JSON_SECRET,
+				{ expiresIn: "1day" }
+			);
 
-		res.json({
-			token: token,
-			user_data: {
-				...req.user._doc,
-			},
-		});
+			res.json({
+				token: token,
+				user_data: {
+					...req.user._doc,
+				},
+			});
+		} catch (err) {
+			console.log("Error occured while Authentication,", err.message);
+			res.status(503).send("SignIn Failed!!");
+		}
 	}
 );
 
